b6542ff3edf49b618238f17c78955354476a8cae
Samantha Fernandes <samanthaascl@gmail.com>
2025-02-09 11:04:54 -0300

    novo

---
+++ outro projeto/app.js
@@ -0,0 +1,8 @@
+let listaGenerica = [];
+let linguagensDeProgramação = ['JavaScript','C','C++', 'Kotlin','Python'];
+linguagensDeProgramação.push('GoLang','Java','Ruby');
+
+//lista com 3 nomes
+let nameList = ['Samantha', 'Theo', 'Thales'];
+console.log(nameList[0]);
+
